be9bb22609e3fbd59b7b003f9e2a01e28a13beb9
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-14 11:16:08 -0300

    first commit

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          CircularProgressIndicator(
+            backgroundColor: Colors.white,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Carregando...',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      )),
+    );
+  }
+}
