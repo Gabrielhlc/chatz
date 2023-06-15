43c2646eab814be953342a9bc016219676ab9123
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-15 11:56:02 -0300

    add: chat_service

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chatz/components/messages.dart';
+import 'package:chatz/components/new_messages.dart';
 import 'package:flutter/material.dart';
 
 import '../core/services/auth/auth_service.dart';
@@ -8,16 +10,41 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Chat page'),
-            TextButton(
-                onPressed: () {
+      appBar: AppBar(
+        title: Text('ChatZ'),
+        actions: [
+          DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Sair'),
+                    ],
+                  ),
+                ),
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
                   AuthService().logout();
-                },
-                child: const Text('Logout'))
+                }
+              }),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: const <Widget>[
+            Expanded(child: Messages()),
+            NewMessages(),
           ],
         ),
       ),
