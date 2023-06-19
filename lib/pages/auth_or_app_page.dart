b3c00c9699221d1e2b889ee23011b36c67d2a746
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-19 14:39:23 -0300

    auth refactor to firebase auth

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -5,25 +5,37 @@ import 'package:chatz/core/models/chat_user.dart';
 import 'package:chatz/pages/auth_page.dart';
 import 'package:chatz/pages/chat_page.dart';
 import 'package:chatz/pages/loading_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import '../core/services/auth/auth_service.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-        stream: AuthService().userChanges,
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const LoadingPage();
-          } else {
-            return snapshot.hasData ? const ChatPage() : const AuthPage();
-          }
-        },
-      ),
+    return FutureBuilder(
+      future: init(context),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingPage();
+        } else {
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingPage();
+              } else {
+                return snapshot.hasData ? const ChatPage() : const AuthPage();
+              }
+            },
+          );
+        }
+      },
     );
   }
 }
