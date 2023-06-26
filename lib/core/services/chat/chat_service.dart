1615dfdc1b1472b739327bf4083dc52d3310693e
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-26 13:44:50 -0300

    add: messages to firebase

--- lib/core/services/chat/chat_service.dart
+++ lib/core/services/chat/chat_service.dart
@@ -1,13 +1,13 @@
 import 'package:chatz/core/models/chat_message.dart';
 import 'package:chatz/core/models/chat_user.dart';
-import 'package:chatz/core/services/chat/chat_mock_service.dart';
+import 'package:chatz/core/services/chat/chat_firebase_service.dart';
 
 abstract class ChatService {
   Stream<List<ChatMessage>> messagesStream();
 
-  Future<ChatMessage> save(String text, ChatUser user);
+  Future<ChatMessage?> save(String text, ChatUser user);
 
   factory ChatService() {
-    return ChatMockService();
+    return ChatFirebaseService();
   }
 }
